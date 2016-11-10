89ba0ab9d8f5ec1e214c5ff90b703c07ca45d390
CiaranCostello <costelci@tcd.ie>
2016-11-10 13:10:09 +0000

    almost finished apart from kill message
    Signed-off-by: CiaranCostello <costelci@tcd.ie>

--- threadedserver.py
+++ threadedserver.py
@@ -1,5 +1,7 @@
 import socket
 import threading
+import sys, argparse, os
+from urllib.request import urlopen
 
 std_ID = 13321463
 
@@ -7,7 +9,7 @@ class Server(object):
 
 	max_workers = 16
 
-	def __init__(self, host, port):
+	def __init__(self, host, port, ip):
 		#bind to port and host specified
 		self.host = host
 		self.port = port
@@ -16,39 +18,63 @@ class Server(object):
 		#create task list and initialise the number of workers
 		self.tasks = []
 		self.no_of_workers = 0
-		self.lock = Lock()
-
+		self.lock = threading.Lock()
+		self.ip = ip
 
 	def listen(self):
-		self.socket.listen()
+		self.socket.listen(5)
 		while True:
 			client, address = self.socket.accept()
+			print("Received connection from {0}".format(address))
 			#add connection to task list
 			self.tasks.append((client, address))
 			#if we aren't at max threads and there are a few tasks waiting in the queue start another worker
 			self.lock.acquire()
-			if(self.no_of_workers < self.max_workers && len(tasks) > 3):
+			if(self.no_of_workers < self.max_workers and len(self.tasks) >= 0):
+				print("Creating worker")
 				self.no_of_workers +=1
 				threading.Thread(target = self.Worker).start()
 			self.lock.release()
 
 	def Worker(self):
-		while len(tasks) > 0:
-			(client, (host, port)) = tasks.pop(0)
+		print("Started worker. There are now {0} workers".format(self.no_of_workers))
+		while len(self.tasks) > 0:
+			(client, (host, port)) = self.tasks.pop(0)
 			size = 1024
-	        while True:
-	            try:
-	                data = client.recv(size)
-	                if data:
-	                	data = data.decode("utf-8")
-	                    # Set the response to echo back the recieved data 
-	                    if data == "HELO text\n":
-	                    	response = "HELO text\nIP:{0}\nPort:{1}\nStudentID:{2}\n".format(host, port, std_ID)
-	                    else:
-	                    	response = data
-	                    client.send(response.encode("utf-8"))
-	                else:
-	                    raise error('Client disconnected')
-	            except:
-	                client.close()
-	                return False
+			while True:
+				try:
+					data = client.recv(size)
+					if data:
+						data = data.decode("utf-8")
+						print("Received message: {0}".format(data))
+						# Set the response to echo back the recieved data
+						if data[0:5] == "HELO " and data[-1] == "\n":
+							print("Adding student id.")
+							response = "{3}IP:{0}\nPort:{1}\nStudentID:{2}\n".format(self.ip, self.port, std_ID, data)
+						else:
+							print("Simply echoing data.")
+							response = data
+						client.send(response.encode("utf-8"))
+						print("Send response")
+					else:
+						raise error('Client disconnected')
+				except:
+					client.close()
+				return False
+
+def clargs():
+	parser = argparse.ArgumentParser(description='TCP thread pool server.')
+	parser.add_argument('-p', '--port', type=int, required=True, help='Port to connect to')
+	parser.add_argument('-o', '--host', required=False, help='Host name to send get request to')
+	parser.add_argument('-i', '--ipAddress', required=True, help='IP address of host')
+	return parser.parse_args()
+
+if __name__ == '__main__':
+	args = clargs()
+	s = Server('', args.port, args.ipAddress)
+	try:
+		s.listen()
+	except KeyboardInterrupt:
+		print("Ctrl C - Stopping server")
+		sys.exit(1)
+	
